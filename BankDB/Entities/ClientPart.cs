c22c50441a9565a0a4c44facc301c5c9e2ee4c99
Tim-git-hub <timothe.rominger@epitech.eu>
2025-03-11 10:51:44 +0100

    Ajout Classe client/ClientPro/ClientPart/CarteBancaire/CompteBancaire/Operation + BankDbContext

---
+++ BankDB/Entities/ClientPart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.Data.Entities
+{
+    public class ClientPart : Client
+    {
+        public DateTime DateNaissance { get; set; }
+
+        public string Prenom { get; set; }
+
+        public char Sexe { get; set; }
+    }
+}
---
+++ BankDB/Entities/CompteBancaire.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.Data.Entities
+{
+    public class CompteBancaire
+    {
+        [Key]
+        public string NumCompte { get; set; }
+
+        public DateTime DateOuverture { get; set; }
+
+        public decimal Solde { get; set; } = 1000.00m;
+
+        [ForeignKey("Client")]
+        public int ClientId { get; set; }
+        public Client Client { get; set; }
+    }
+}
