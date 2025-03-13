3a12afa2bad562c202775856438e839750cc2121
Nathan <61934160+OjjyCoco@users.noreply.github.com>
2025-03-13 10:29:42 +0100

    Client Final Menu with Login

---
+++ Client/Client.Menu/AuthService.cs
@@ -0,0 +1,37 @@
+﻿using Recap.Datas.Repositories;
+using System;
+using System.Threading.Tasks;
+
+namespace Client.Menu
+{
+    public class AuthService
+    {
+        private readonly UtilisateurRepository _userRepo;
+
+        public AuthService()
+        {
+            _userRepo = new UtilisateurRepository();
+        }
+
+        public async Task<(bool, int?)> Authentifier(string login, string password)
+        {
+            var user = await _userRepo.GetByLogin(login);
+            if (user == null)
+            {
+                Console.WriteLine("Utilisateur introuvable.");
+                return (false, null);
+            }
+
+            // Comparer le mot de passe (hashé idéalement)
+            if (user.Password != password)
+            {
+                Console.WriteLine("Mot de passe incorrect.");
+                return (false, null);
+            }
+
+            Console.WriteLine($"Connexion réussie ! Bienvenue {user.Login} !");
+            Console.ReadKey();
+            return (true, user.ClientId);
+        }
+    }
+}
---
+++ Client/Client.Menu/Menu.cs
@@ -0,0 +1,54 @@
+﻿//using System;
+//using System.Threading.Tasks;
+//using Bank.Controllers;
+//using System.Threading.Tasks;
+
+
+//namespace Client.Menu
+//{
+//    class Menu
+//    {
+//        private static async Task Main(string[] args)
+//        {
+//            var operationController = new OperationController();
+//            var clientController = new ClientController();
+
+//            bool exit = false;
+
+//            while (!exit)
+//            {
+//                Console.Clear();
+//                Console.WriteLine("===== MENU PRINCIPAL =====");
+//                Console.WriteLine("1 - Consulter les opérations");
+//                Console.WriteLine("2 - Consulter les clients");
+//                Console.WriteLine("0 - Quitter");
+//                Console.Write("Choisissez une option : ");
+
+//                string? choix = Console.ReadLine();
+
+//                switch (choix)
+//                {
+//                    case "1":
+//                        await operationController.AfficherToutesLesOperations();
+//                        break;
+//                    case "2":
+//                        await clientController.AfficherTousLesClients();
+//                        break;
+//                    case "0":
+//                        exit = true;
+//                        Console.WriteLine("Fermeture de l'application...");
+//                        break;
+//                    default:
+//                        Console.WriteLine("Option invalide, veuillez réessayer.");
+//                        break;
+//                }
+
+//                if (!exit)
+//                {
+//                    Console.WriteLine("\nAppuyez sur une touche pour revenir au menu...");
+//                    Console.ReadKey();
+//                }
+//            }
+//        }
+//    }
+//}
